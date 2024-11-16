6beb1dbed36d5c3d4990a43c5c3829fd74cd0d42
santiagomtz12 <santiago_mtz12@yahoo.com>
2024-11-15 20:29:11 -0600

    420054700

--- ProyectoFinal/ProyectoFinal.cpp
+++ ProyectoFinal/ProyectoFinal.cpp
@@ -37,7 +37,7 @@ const GLuint WIDTH = 800, HEIGHT = 600;
 int SCREEN_WIDTH, SCREEN_HEIGHT;
 
 // Camera
-Camera  camera(glm::vec3(0.0f, 20.0f, -100.0f));
+Camera  camera(glm::vec3(0.0f, 20.0f, -150.0f));
 GLfloat lastX = WIDTH / 2.0;
 GLfloat lastY = HEIGHT / 2.0;
 bool keys[1024];
